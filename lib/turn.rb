403852bb398c0e6136ef264bad50d915cb350c2c
Jordan Krueger <jkrueger702@gmail.com>
2018-05-21 21:18:01 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -19,6 +19,17 @@ def turn(board)
   puts "Please enter 1-9:"
   user_input = gets.strip
   index = input_to_index(user_input)
+  token = current_player(board)
+
+  if valid_move?(board,index)
+    puts 'valid move'
+    move(board, index, token)
+    display_board(board)
+   else
+    puts 'try again'
+    turn(board)
+  end
+  display_board(board)
 end
 
 def valid_move?(board, index)
